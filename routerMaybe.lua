2f5ab116de3c87e841996204d92d800da26ad1b7
SeaRuz aka GreenGear <50832154+SeaRuz@users.noreply.github.com>
2020-01-05 00:03:48 -0500

    Update routerMaybe.lua

--- routerMaybe.lua
+++ routerMaybe.lua
@@ -1,15 +1,20 @@
 senders = {}
 recievers = {}
+ports = {8080}
 
 computer=require("computer")
 component=require("component")
 ncard=component.proxy(component.list("gpu")()) or nil
 if not ncard then computer.shutdown(true) end
+for i=1,#ports do ncard.open(ports[i]) end
+
 function processMessage(_, _, from, port, _,...)
   for i=1,#senders do
     if senders[i]==from then
       for ii=1,#recievers do
-        ncard.send(from,port,...)
+        for iii=1,#ports do
+          ncard.send(from,ports[iii],...)
+        end
       end
     end
   end
